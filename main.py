ea8780c5bba269f3a76c4a508a3a5b54ef26435e
basalcode <basalcode@gmail.com>
2022-03-01 13:47:14 +0900

    Extract data from page

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-indded_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
 # print(indded_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+
+pagination_list = indeed_soup.find("ul", {"class": "pagination-list"})
+
+links = pagination_list.find_all("a")
+
+pages = []
+
+for link in links[:-1]:
+    pages.append(int(link.string))
+
+print(pages)
