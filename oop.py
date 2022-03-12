a7ade74d4925d2584c1badfe3a7d7bf408feffd8
basalcode <basalcode@gmail.com>
2022-03-12 20:53:33 +0900

    Create python OOP example

---
+++ oop.py
@@ -0,0 +1,20 @@
+class Car():
+    wheels = 4
+    doors = 4
+    windows = 4
+    seats = 4
+    def start(this):
+        print(this)
+        print("I started")
+
+porche = Car()
+porche.color = "Red"
+porche.start()
+ 
+
+ferrari = Car()
+ferrari.color = "Yellow"
+
+mini = Car()
+mini.color = "White"
+
