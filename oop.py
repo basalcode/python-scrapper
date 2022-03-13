92075f11d2e8b9337f2fcbaf2f99b0ade5fd019b
basalcode <basalcode@gmail.com>
2022-03-13 14:15:06 +0900

    Update oop example

--- oop.py
+++ oop.py
@@ -1,16 +1,18 @@
 class Car():
-    wheels = 4
-    doors = 4
-    windows = 4
-    seats = 4
-    def start(this):
-        print(this)
-        print("I started")
+    def __init__(self, **keyword_arguments):
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = keyword_arguments.get("color", "black")
+        self.price = keyword_arguments.get("price", "$20")
+    
+    def __str__(self):
+        return f"Car with {self.wheels} wheels"
 
-porche = Car()
-porche.color = "Red"
-porche.start()
- 
+
+porche = Car(color="green", price="$40")
+print(porche.color, porche.price)
 
 ferrari = Car()
 ferrari.color = "Yellow"
