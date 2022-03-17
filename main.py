41853da8e1355828e380b4ec442e43618f938d82
basalcode <basalcode@gmail.com>
2022-03-17 23:59:02 +0900

    Create new route which parsing query

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from encodings import search_function
 from pydoc import render_doc
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, render_template_string, request
 
 app = Flask("SuperScrapper")
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
 
 @app.route("/<username>")
 def contact(username):
